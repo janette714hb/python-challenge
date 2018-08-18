3fc64da155d569814e2cdc757948e53b147162eb
Janette Bennett <Janette.Bennett@lindora.com>
2018-08-18 16:21:04 -0700

    pybank.py

--- PyBank/Pybank.py
+++ PyBank/Pybank.py
@@ -37,10 +37,15 @@ greatest_increase = sorted(data.values(), reverse=True)[0]
 greatest_increase_key = sorted(data, key=data.get, reverse=True)[0]
 print("Greatest Increase in Profits: " + greatest_increase_key + " ($" + str(greatest_increase) + ")")
 
-
 # The greatest decrease in profits (date and amount) over the entire period
 greatest_decrease = sorted(data.values(), reverse=False)[0]
 greatest_decrease_key = sorted(data, key=data.get, reverse=False)[0]
 print("Greatest Decrease in Profits: " + greatest_decrease_key + " ($" + str(greatest_decrease) + ")")
 
 # write file
+f = open("pybank.txt","w")   #create add file in write mode
+f.write("Total: $" + str(sum(data.values())))
+f.write("Average Change: $" + str(sum(data.values()) / len(data.keys())))
+f.write("Greatest Increase in Profits: " + greatest_increase_key + " ($" + str(greatest_increase) + ")")
+f.write("Greatest Decrease in Profits: " + greatest_decrease_key + " ($" + str(greatest_decrease) + ")")
+f.close() #close file
--- PyPoll/pypoll.py
+++ PyPoll/pypoll.py
@@ -31,7 +31,7 @@ print("----------------------------")
 # print total for each candidate, wtih percentage and total votes
 Khan = candidate.count("Khan")
 khanpercent = int((Khan) / len(candidate) * 100)
-print("Kahn: " + str(khanpercent) + "% (" + str(Khan) + ")")    
+print("Khan: " + str(khanpercent) + "% (" + str(Khan) + ")")
 
 Correy = candidate.count("Correy")
 correypercent = int((Correy) / len(candidate) * 100)
@@ -47,5 +47,19 @@ print("O'Tooley " + str(OTooleypercent) + "% (" + str(OTooley) + ")")
 
 print("----------------------------")
 
+if khanpercent > .50:
+	print("Winner: Khan")
+elif correypercent > .50:
+	print("Winner: Correy")
+elif Lipercent > .50:
+	print("Winner: Li")
+else:
+	print("Winner: O'Tooley")			
 
 # write file
+f = open("pypoll.txt","w")   #create add file in write mode
+f.write("Khan: " + str(khanpercent) + "% (" + str(Khan) + ")")
+f.write("Corry: " + str(correypercent) + "% (" + str(Correy) + ")")
+f.write("Li: " + str(Lipercent) + "% (" + str(Li) + ")")
+f.write("O'Tooley " + str(OTooleypercent) + "% (" + str(OTooley) + ")")
+f.close()			#closing file object
